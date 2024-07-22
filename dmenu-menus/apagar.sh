7be2b5ac02f16661e745337ff6235f0159033a3c
TakYzGG <157247602+TakYzGG@users.noreply.github.com>
2024-07-22 11:30:06 -0300

    Add files via upload

---
+++ dmenu-menus/apagar.sh
@@ -0,0 +1,15 @@
+#/bin/sh
+# Creador: TakYzGG
+
+opciones="Apagar\nReiniciar\nCancelar"
+selec=$(echo $opciones | dmenu -i -p "Elige una opcion" -fn "UbuntuMonoNerdFontMono-12" -nb "#2E3440" -nf "#4C566A" -sb "#5E81AC" -sf "#D8DEE9")
+case $selec in
+	Apagar)
+		password=$(echo ":v"| dmenu -i -p "Ingresa la contraseña" -fn "UbuntuMonoNerdFontMono-12" -nb "#2E3440" -nf "#4C566A" -sb "#5E81AC" -sf "#D8DEE9")
+		echo $password | sudo -S poweroff ;;
+	Reiniciar)
+		password=$(echo ":v"| dmenu -i -p "Ingresa la contraseña" -fn "UbuntuMonoNerdFontMono-12" -nb "#2E3440" -nf "#4C566A" -sb "#5E81AC" -sf "#D8DEE9")
+		echo $password | sudo -S reboot ;;
+	Cancelar)
+		exit 0 ;;
+esac
--- dmenu-menus/programas.sh
+++ dmenu-menus/programas.sh
@@ -5,92 +5,68 @@ opciones="Lxterminal\nFirefox\nGestor de archivos\nAccesorios\nMultimedia\nLibre
 selec=$(echo $opciones | dmenu -i -p "Elige una opcion" -fn "UbuntuMonoNerdFontMono-12" -nb "#2E3440" -nf "#4C566A" -sb "#5E81AC" -sf "#D8DEE9")
 case $selec in
 	Lxterminal)
-		lxterminal
-		;;
+		lxterminal ;;
 	Firefox)
-		firefox
-		;;
+		firefox ;;
 	"Gestor de archivos")
-		Thunar
-		;;
+		Thunar ;;
 
 	Accesorios)
 		accesorios="Calculadora\nVisor de PDF\nEditor de texto\nGestor de discos\nVisor de imagenes\nGestor de archivos\nControlador de volumen\nCancelar"
 		selec=$(echo $accesorios | dmenu -i -p "Elige una opcion" -fn "UbuntuMonoNerdFontMono-12" -nb "#2E3440" -nf "#4C566A" -sb "#5E81AC" -sf "#D8DEE9")
 		case $selec in
 			Calculadora)
-				galculator
-				;;
+				galculator ;;
 			"Visor de PDF")
-				xreader
-				;;
+				xreader ;;
 			"Editor de texto")
-				leafpad
-				;;
+				leafpad ;;
 			"Gestor de discos")
-				gparted
-				;;
+				password=$(echo ":v"| dmenu -i -p "Ingresa la contraseña" -fn "UbuntuMonoNerdFontMono-12" -nb "#2E3440" -nf "#4C566A" -sb "#5E81AC" -sf "#D8DEE9")
+				echo $password | sudo -S gparted ;;
 			"Visor de imagenes")
-				mirage
-				;;
+				mirage ;;
 			"Gestor de archivos")
-				Thunar
-				;;
+				Thunar ;;
 			"Compresor de archivos")
-				xarchiver
-				;;
+				xarchiver ;;
 			"Controlador de volumen")
-				pavucontrol
-				;;
+				pavucontrol ;;
 			Cancelar)
-				exit 0
-				;;
-		esac
-		;;
+				exit 0 ;;
+		esac ;;
 
 	Multimedia)
 		multimedia="Reproductor de video\nReproductor de musica\nCancelar"
 		selec=$(echo $multimedia | dmenu -i -p "Elige una opcion" -fn "UbuntuMonoNerdFontMono-12" -nb "#2E3440" -nf "#4C566A" -sb "#5E81AC" -sf "#D8DEE9")
 		case $selec in
 			"Reproductor de video")
-				vlc
-				;;
+				vlc ;;
 			"Reproductor de musica")
-				audacious
-				;;
+				audacious ;;
 			Cancelar)
-				exit 0
-				;;
-		esac
-		;;
+				exit 0 ;;
+		esac ;;
 
 	"Libre Office")
 		office="Word\nExel\nDibujo\nPowerpoint\nBases de datos\nEditor de formulas\nCancelar"
 		selec=$(echo $office | dmenu -i -p "Elige una opcion" -fn "UbuntuMonoNerdFontMono-12" -nb "#2E3440" -nf "#4C566A" -sb "#5E81AC" -sf "#D8DEE9")
 		case $selec in
 			Word)
-				libreoffice --writer
-				;;
+				libreoffice --writer ;;
 			Exel)
-				libreoffice --calc
-				;;
+				libreoffice --calc ;;
 			Dibujo)
-				libreoffice --draw
-				;;
+				libreoffice --draw ;;
 			Powerpoint)
-				libreoffice --impress
-				;;
+				libreoffice --impress ;;
 			"Bases de datos")
-				libreoffice --base
-				;;
+				libreoffice --base ;;
 			"Editor de formulas")
-				libreoffice --math
-				;;
+				libreoffice --math ;;
 			Cancelar)
-				exit 0
-				;;
-		esac
-		;;
+				exit 0 ;;
+		esac ;;
 
 	Cancelar)
 		exit 0
